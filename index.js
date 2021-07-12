d8777a8b0485db5ca696d73efdcc79b6b4065466
Dan Boudet <dboudet04@gmail.com>
2021-07-12 16:04:48 -0400

    added search by query parameter

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express') // uses express library
 const cors = require('cors')  // users cors library (cross-origin resource sharing)
-const { getCustomers, getCustomerById } = require('./src/customers')  // pulls in functions from source file
+const { getCustomers, getCustomerById, getCustomerByQuery } = require('./src/customers')  // pulls in functions from source file
 const app = express()  // allows us to use 'app' to call express functions
 app.use(cors())  // use cors
 
-app.get('/customers/:id', getCustomerById) // get request to call function to get customer by id
+app.get('/customers/search', getCustomerByQuery)
+app.get('/customers/:customerId', getCustomerById) // get request to call function to get customer by id
 app.get('/customers', getCustomers)   // get request to call the getCustomers function when accessing the /customers route
 // app.post('/customers', createCustomer)
 
--- src/customers.js
+++ src/customers.js
@@ -1,4 +1,5 @@
 
+const { request, response } = require('express')
 const admin = require('firebase-admin')
 const credentials = require('../credentials.json')
 
@@ -26,15 +27,15 @@ exports.getCustomers = (request, response) => {
 }
 
 exports.getCustomerById = (request, response) => {
-    if (!request.params.id) {
+    if (!request.params.customerId) {
         response.status(400).send('No customer specified!.')
         return
     }
     const db = connectDb()
-    db.collection('customers').doc(request.params.id).get()
-        .then( doc => {
-            const customer = doc.data()
-            customer.id = doc.id   
+    db.collection('customers').doc(request.params.customerId).get()
+        .then( customerDoc => {
+            let customer = customerDoc.data()
+            customer.id = customerDoc.id
                 /* *** important to set id as property of object because firestore doesn't 
                 automatically do this. so this pulls the doc id (name of object itself) and 
                 assigns it as a property of the doc. */
@@ -46,3 +47,24 @@ exports.getCustomerById = (request, response) => {
         })
 }
 
+// return results based on query parameter (fname) in URL/path
+exports.getCustomerByQuery = (req,res) => {
+    //get query from req.query
+    const { fname } = req.query
+    // connect to firestore
+    const db = connectDb()
+    // search customers collection based on query
+    db.collection('customers').where( 'firstName', '==', fname ).get()
+    // respond with results
+        .then(
+            customerCollection => {
+                const matches = customerCollection.docs.map( doc => {
+                    let customer = doc.data()
+                    customer.id = doc.id
+                    return customer
+                })
+                res.send(matches)
+            }
+        )
+        .catch(err => res.status(500).send(err))
+}
