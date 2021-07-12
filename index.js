24f8e7e3dbfba4e1266e90f06a61d24bf0104137
Dan Boudet <dboudet04@gmail.com>
2021-07-12 14:45:46 -0400

    added function to get single customer

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express')
-const cors = require('cors')
-const { getCustomers } = require('./src/customers')
-const app = express()
-app.use(cors())
+const express = require('express') // uses express library
+const cors = require('cors')  // users cors library (cross-origin resource sharing)
+const { getCustomers, getCustomerById } = require('./src/customers')  // pulls in functions from source file
+const app = express()  // allows us to use 'app' to call express functions
+app.use(cors())  // use cors
 
-app.get('/customers', getCustomers)
+app.get('/customers/:id', getCustomerById) // get request to call function to get customer by id
+app.get('/customers', getCustomers)   // get request to call the getCustomers function when accessing the /customers route
 // app.post('/customers', createCustomer)
-
-
 
 
 app.listen(3000, () => {
--- src/customers.js
+++ src/customers.js
@@ -24,3 +24,25 @@ exports.getCustomers = (request, response) => {
             response.status(500).send(err)
         })
 }
+
+exports.getCustomerById = (request, response) => {
+    if (!request.params.id) {
+        response.status(400).send('No customer specified!.')
+        return
+    }
+    const db = connectDb()
+    db.collection('customers').doc(request.params.id).get()
+        .then( doc => {
+            const customer = doc.data()
+            customer.id = doc.id   
+                /* *** important to set id as property of object because firestore doesn't 
+                automatically do this. so this pulls the doc id (name of object itself) and 
+                assigns it as a property of the doc. */
+            response.send(customer)
+        })
+        .catch( err => {
+            console.error(err)
+            response.status(500).send(err)
+        })
+}
+
